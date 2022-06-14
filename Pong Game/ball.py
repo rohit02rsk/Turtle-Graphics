6a7d4846ea1880a089d361377470ad9ed7e52360
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2022-06-14 18:36:02 +0530

    Add files via upload

---
+++ Pong Game/ball.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("green")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+    
+    def bounce_wall(self):
+        self.y_move *= -1
+
+    def bounce_paddle(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.move_speed = 0.1
+        self.goto(0, 0)
+        self.bounce_paddle()
